6162fc19744c0466b00910c15e547d070fc4421b
avsg1991@gmail.com <avsg1991@gmail.com>
2026-01-20 13:18:34 -0500

    AppModule mysql db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'taskflow_db',
+      autoLoadEntities: true, // Carga automáticamente las entidades que crearemos
+      synchronize: true, // Solo para desarrollo: crea las tablas automáticamente
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
